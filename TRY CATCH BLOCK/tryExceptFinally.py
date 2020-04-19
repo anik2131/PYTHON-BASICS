653d7ac2c79ad5f1fab25e57d234789c5efe2f4c
anik2131 <52519854+anik2131@users.noreply.github.com>
2020-04-20 03:48:04 +0530

    Update tryExceptFinally.py

--- TRY CATCH BLOCK/tryExceptFinally.py
+++ TRY CATCH BLOCK/tryExceptFinally.py
@@ -1,10 +1,10 @@
-try:
+try: # exception to be handled
   number1=int(input("Enter first number: "))
   number2=int(input("Enter second number: "))
   print(number1/number2)
 except ValueError:
   print("Invalid input")
-except ZeroDivisionError:
+except ZeroDivisionError:  #Exception caught
   print("Divide by Zero Error")
-finally:
+finally: # it is always executed irrespective of try-except block
   print("Try-Except Block executed Successfully")
