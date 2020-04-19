2bfc0069e527147c6636f0965b5e940ebe52cfde
anik2131 <52519854+anik2131@users.noreply.github.com>
2020-04-20 01:06:55 +0530

    Add files via upload

---
+++ BASIC DATATYPES/basicDatatypes.py
@@ -0,0 +1,16 @@
+intA=21
+floatA=2.07
+doubleA=2100000096789283765.029375237298
+charA='x'
+stringA="Hello World"
+longA=987263782163498147891274198
+shortA=12
+listA=[1,2,3,"a","b","c","Hello","World"]
+print(intA)
+print(floatA)
+print(doubleA)
+print(charA)
+print(stringA)
+print(longA)
+print(shortA)
+print(listA)
