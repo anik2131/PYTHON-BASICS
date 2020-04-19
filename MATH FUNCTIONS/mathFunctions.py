37398f3515e4bc4dc31449cfeca9ea7be07241ef
anik2131 <52519854+anik2131@users.noreply.github.com>
2020-04-20 00:36:41 +0530

    Add files via upload

---
+++ MATH FUNCTIONS/mathFunctions.py
@@ -0,0 +1,10 @@
+from math import *
+
+print(max(4,5)) # return the maximum of two values
+print(min(2.5,2.6)) # return the minimum of two values
+print(sqrt(36)) # return the square root of a number
+print(round(27.4)) # return the round-off value of a number
+print(round(29.5)) # if >=0.5 then the corresponding next number
+print(floor(27.5)) # returns the number irrespective of the decimal value 
+print(ceil(27.4)) # returns the next corresponding number irrespective of the decimal value
+print(pow(2,3)) # returns the value of x^y
